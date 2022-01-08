68c761f70018edaef98e273044ee2090187bf614
dimadeloseros1 <dimadeloseros1@gmail.com>
2022-01-08 00:45:31 +0000

    update

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 // MongoDB Connection: mongodb+srv://dimadeloseros1:<password>@cluster0.huawn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
 
 
 const app = express();
 
-mongoose.connect('mongodb+srv://dimadeloseros1:fgmkiller12@cluster0.huawn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://dimadeloseros1:fgmkiller12@cluster0.huawn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true, }
+)
     .then(() => {console.log('Successfully connected to MongoDb Atlas!')})
     .catch((error) => {console.log('Unable to connect to MongoDb Atlas!'); console.error(error);});
     
@@ -20,35 +23,10 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.search(bodyParser.json());
+  
 
-app.post('/api/sauces', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Thing saved successfully!'
-    });
-})
 
-app.get('/api/sauces', (req, res, next) => {
-    const stuff = [
-       {
-        _id: 'Something',
-        title: 'Something',
-        description: 'All of the info about my frist thing',
-        imageUrl: '',
-        price: 4900,
-        userId: 'something',
-    },
-    {
-        _id: 'Something',
-        title: 'Something1',
-        description: 'All of the info about my frist thing',
-        imageUrl: '',
-        price: 4900,
-        userId: 'something',
-    },
-   ];
-   res.status(200).json(stuff);
-});
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;

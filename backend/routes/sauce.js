400a7ca7b41753026f3dc0f584155c1e1261b0aa
dimadeloseros1 <dimadeloseros1@gmail.com>
2022-01-08 12:55:35 +0000

    half way done

---
+++ backend/routes/sauce.js
@@ -0,0 +1,18 @@
+const express = require('expres');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const sauceCtrl = require('../controllers/sauce');
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
